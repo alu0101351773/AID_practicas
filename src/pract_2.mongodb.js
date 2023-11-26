d957f31c2b9ffbf8c4430c95973724789b96f134
Jorge Cabrera <alu0101351773@ull.edu.es>
2023-11-26 16:49:10 +0000

    cambios menores a las consultas para poder ejecutarlas de forma individual

--- src/pract_2.mongodb.js
+++ src/pract_2.mongodb.js
@@ -4,14 +4,17 @@
 use('AID-Mongo');
 
 // 1. Cuantos documentos tiene la colección personas
+use('AID-Mongo');
 db.personas.find({}).count()
 
 
 // 2. Contenido completo de la colección personas
+use('AID-Mongo');
 db.personas.find({})
 
 
 // 3. Cambiar el DNI de "Milagros Perdomo Gómez" a 1020
+use('AID-Mongo');
 db.personas.updateOne(
     { 
         $and: [
@@ -26,17 +29,22 @@ db.personas.updateOne(
     }
 )
 
+db.personas.find({DNI: 1020})
+
 
 // 4. Lista el contenido de la colección personas ordenada de forma ascendente
 // por la edad
+use('AID-Mongo');
 db.personas.find({}).sort({ Edad: 1 })
 
 
 // 5. Idem que la anterior pero en formato elegante
+use('AID-Mongo');
 db.personas.find({}).pretty().sort({ Edad: 1 })
 
 
 // 6. Muestra todos los datos de la persona con DNI 3333.
+use('AID-Mongo');
 db.personas.find({
     DNI: 3333
 }).projection({
@@ -45,6 +53,7 @@ db.personas.find({
 
 
 // 7. Muestra el DNI, nombre y apellidos de las personas que vivan en Santa Cruz
+use('AID-Mongo');
 db.personas.find({
     Ciudad: "Santa Cruz"
 }).projection({
@@ -56,6 +65,7 @@ db.personas.find({
 
 
 // 8. Muestra el DNI de las personas que viven en Santa Cruz o La Laguna
+use('AID-Mongo');
 db.personas.find({
     $or: [
         { Ciudad: "Santa Cruz" },
@@ -68,6 +78,7 @@ db.personas.find({
 
 
 // 9. Muestra el nombre y apellidos de las personas con edades entre 19 y 25 años
+use('AID-Mongo');
 db.personas.find({
     $and: [
         { Edad: { $gte: 19 } },
@@ -83,6 +94,7 @@ db.personas.find({
 
 // 10. Muestra el DNI, nombre y apellidos de las personas a las que les gusta
 // el running y la natación.
+use('AID-Mongo');
 db.personas.find({
     $and: [
         { Aficiones: { $in: ["running"] } },
@@ -98,6 +110,7 @@ db.personas.find({
 
 // 11. Muestra el DNI, nombre y apellidos de las personas a las que les gusta
 // el running o la natación.
+use('AID-Mongo');
 db.personas.find({
     Aficiones: { $in: ["running", "natacion"] }
 }).projection({
@@ -109,17 +122,18 @@ db.personas.find({
 
 
 // 12. Muestra los DNI de las personas que son amigas de la persona con DNI 5555.
+use('AID-Mongo');
 db.personas.find({
     Amigos: { $in: [{DNI: 5555}] }
 }).projection({
     _id: 0,
-    DNI: 1,
-    Amigos: 1
+    DNI: 1
 })
 
 
 // 13. Muestra un listado de las aficiones que tienen las personas con edades
 // inferiores a 22 años.
+use('AID-Mongo');
 db.personas.find({
     Edad: { $lt: 22 }
 }).projection({
@@ -129,18 +143,21 @@ db.personas.find({
 
 
 // 14. ¿Cuántas personas viven en Santa Cruz?
+use('AID-Mongo');
 db.personas.countDocuments({
     Ciudad: "Santa Cruz"
 })
 
 
 // 15. ¿Cuántos hombres hay en nuestra base de datos?
+use('AID-Mongo');
 db.personas.countDocuments({
     Sexo: 'H'
 })
 
 
 // 16. ¿Cuántas personas viven en cada ciudad?
+use('AID-Mongo');
 db.personas.aggregate([{
     $group: {
         _id: "$Ciudad",
@@ -152,6 +169,7 @@ db.personas.aggregate([{
 
 
 // 17. ¿Cuántas personas hay de cada sexo en nuestra base de datos?
+use('AID-Mongo');
 db.personas.aggregate([{
     $group: {
         _id: "$Sexo",
@@ -163,6 +181,7 @@ db.personas.aggregate([{
 
 
 // 18. Muestra para cada persona cuántos amigos tiene
+use('AID-Mongo');
 db.personas.aggregate([{
     $project: {
         _id: 0,
@@ -174,20 +193,24 @@ db.personas.aggregate([{
 
 
 // 19. Añade a la lista de aficiones de la persona con DNI 9999 el bricolaje
+use('AID-Mongo');
 db.personas.updateOne(
     { DNI: 9999 },
     { $addToSet: { Aficiones: "bricolaje" } }
 )
+db.personas.find({DNI: 9999})
 
 
 // 20. Incrementa la edad de la persona con DNI 3333 en 2 años
+use('AID-Mongo');
 db.personas.updateOne(
     { DNI: 3333 },
     { $inc: { Edad: 2 } }
 )
-
+db.personas.find({DNI: 3333})
 
 // 21. Muestra los DNI y edades de todos los documentos de la colección personas
+use('AID-Mongo');
 db.personas.aggregate([{
     $project: {
         _id: 0,
@@ -200,6 +223,7 @@ db.personas.aggregate([{
 // 22. Modifica los siguientes campos para las personas que vivan en La Laguna:
 //     - Multiplica su edad por 2
 //     - Añadeles un campo denominado Ocupación con el valor “Estudiante”.
+use('AID-Mongo');
 db.personas.updateMany(
     { Ciudad: "La Laguna" },
     {
@@ -210,15 +234,18 @@ db.personas.updateMany(
 
 
 // 23. Muestra todos los datos de las personas que viven en La Laguna
+use('AID-Mongo');
 db.personas.find({ Ciudad: "La Laguna" })
 
 
 // 24. Elimina las personas que vivan en El Sauzal
+use('AID-Mongo');
 db.personas.deleteMany({ Ciudad: "El Sauzal" })
-
+// db.personas.find({ Ciudad: "El Sauzal"})
 
 // 25. Muestra el contenido de la colección personas ordenado de forma
 // descendente por ciudad
+use('AID-Mongo');
 db.personas.aggregate([{
     $project: {
         _id: 0,
@@ -236,6 +263,7 @@ db.personas.aggregate([{
 
 
 // 26. Elimina las personas que tengan un solo amigo
+use('AID-Mongo');
 db.personas.deleteMany({
     Amigos: { $size: 1 }  
 })
@@ -243,6 +271,7 @@ db.personas.deleteMany({
 
 // 27. Muestra el contenido de la colección personas ordenado de forma
 // descendente por ciudad y ascendente por los campos apellidos y nombre
+use('AID-Mongo');
 db.personas.aggregate([{
     $project: {
         _id: 0,
